ad0fbc3f8fc889abb43556b7675f8f96c2cc8d4b
Jonas <jonasdr.jdr@gmail.com>
2022-03-16 09:58:16 +0100

    Test menu

---
+++ test/My project (3)/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public Panel currentPanel = null;
+
+    private List<Panel> panelHistory = new List<Panel>();
+    // Start is called before the first frame update
+    private void Start()
+    {
+        SetupPanels();
+    }
+
+    private void SetupPanels()
+    {
+        Panel[] panels = GetComponentsInChildren<Panel>();
+
+        foreach (Panel panel in panels)
+            panel.Setup(this);
+
+        currentPanel.Show();
+    }
+}
